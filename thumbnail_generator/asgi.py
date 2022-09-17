21fa586ea64442e333607c645208d36736f0e237
shhhQuiettt <krzysztof.skro@gmail.com>
2022-09-17 13:41:57 +0200

    Initial commit

---
+++ thumbnail_generator/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for thumbnail_generator project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thumbnail_generator.settings')
+
+application = get_asgi_application()
